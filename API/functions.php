711e389a23df1afae9aeceec2e403c21b810bf51
Marc Wensauer <git@ansolas.de>
2019-06-06 13:59:18 +0200

    that works better

--- API/functions.php
+++ API/functions.php
@@ -6,27 +6,20 @@
 $api="https://api.steemit.com";
 
 // Curl Method //
+function GetData( $api, $pdata )
+{ 
+    $ch = curl_init($api); // steemit api
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $pdata); //post data
+    curl_setopt($ch, CURLOPT_HTTPHEADER, false); // http header request
+    $data = curl_exec($ch);//get data from steemit API
+    curl_close($ch); //close curl
 
-function GetData($api,$pdata){
-
-$ch = curl_init($api);
-     // steemit api
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $pdata);
-  //post data
-curl_setopt($ch, CURLOPT_HTTPHEADER, false);
- // http header request
-$data = curl_exec($ch);
-     //get data from steemit API
-curl_close($ch);
-  
-   //close curl
-
-$arr = json_decode($data,true);
-      //decode data 
-$info = $arr[result];      //select result array from JSON 
-
+    $arr = json_decode($data,true); //decode data 
+    $info = $arr['result']; //select result array from JSON
+    
+    return $info;
 }
 
